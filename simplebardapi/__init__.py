622112f95f98f14ed3f38364bdb4b96c2e80c7a1
Ruu3f <78753690+Ruu3f@users.noreply.github.com>
2023-09-09 15:08:10 +0530

    Fixed, simplified

--- simplebardapi/__init__.py
+++ simplebardapi/__init__.py
@@ -1,42 +1,30 @@
 __author__ = "Ruu3f"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
-from re import search
+
 from string import digits
 from random import choices
-from requests import Session
+from re import search, error
 from json import dumps, loads
+from requests import Session, exceptions
 
 
 class Bard:
     """
-    A class that interacts with the Google Bard to generate answers to user prompts.
+    Bard class for interacting with the Google Bard API.
 
     Args:
-        cookie (str): The value of the __Secure-1PSID cookie for authentication.
-        proxies (str, optional): Proxy configuration for HTTP requests. Default is None.
+        cookie (str): The value of the '__Secure-1PSID' cookie.
+        proxies (str, optional): The proxies to use for requests, if any.
 
-    Attributes:
-        proxies (str): Proxy configuration for HTTP requests.
-        cookie (str): The value of the __Secure-1PSID cookie for authentication.
-        session (Session): The HTTP session used for making requests to the Bard API.
-        snlm0e (str): The value of the SNlM0e parameter extracted from the Secure_1PSID cookie.
-
-    Methods:
-        generate_answer(prompt): Generates an answer for the given user prompt.
-        _create_session(): Creates an HTTP session with necessary headers and cookies.
-        _get_snlm0e(): Extracts the SNlM0e value from the Secure_1PSID cookie.
-
+    Attr:
+        cookie (str): The value of the '__Secure-1PSID' cookie.
+        proxies (str, optional): The proxies to use for requests, if any.
+        session (requests.Session): A session object for making HTTP requests.
+        snlm0e (str): The 'SNlM0e' value obtained from the website.
     """
 
     def __init__(self, cookie: str, proxies: str = None):
-        """
-        Initialize the Bard instance.
-
-        Args:
-            cookie (str): The value of the __Secure-1PSID cookie for authentication.
-            proxies (str, optional): Proxy configuration for HTTP requests. Default is None.
-        """
         self.proxies = proxies
         self.cookie = cookie
         self.session = self._create_session()
@@ -44,74 +32,48 @@ class Bard:
 
     def generate_answer(self, prompt):
         """
-        Generate an answer for the given user prompt.
+        Generate an answer to a given prompt.
 
         Args:
-            prompt (str): The user's prompt for which an answer is to be generated.
+            prompt (str): The input prompt for generating an answer.
 
         Returns:
-            dict: A dictionary containing the generated answer and related information.
+            dict: A dictionary containing the generated answer, conversation ID, response ID,
+            available choices, and images (if any).
         """
-        request_id = int("".join(choices(digits, k=4)))
-        params = {
-            "bl": "boq_assistant-bard-web-server_20230713.13_p0",
-            "_reqid": str(request_id),
-            "rt": "c",
-        }
-        prompt_struct = [[prompt], None, [None, "", ""]]
-        data = {
-            "f.req": dumps(["", dumps(prompt_struct)]),
-            "at": self.snlm0e,
-        }
         resp = self.session.post(
             "https://bard.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate",
-            params=params,
-            data=data,
-            timeout=30,
+            params={
+                "bl": "boq_assistant-bard-web-server_20230713.13_p0",
+                "_reqid": int("".join(choices(digits, k=4))),
+                "rt": "c",
+            },
+            data={
+                "f.req": dumps(["", dumps([[prompt], None, [None, "", ""]])]),
+                "at": self.snlm0e,
+            },
+            timeout=15,
+            proxies=self.proxies,
         )
         parsed_answer = loads(loads(resp.content.splitlines()[3])[0][2])
-        images = (
-            {img[0][0][0] for img in parsed_answer[4][0][4]}
-            if parsed_answer[4][0][4]
-            else set()
-        )
-        links = [
-            item
-            for current in parsed_answer[4]
-            if isinstance(current, list)
-            for item in current
-            if isinstance(item, str)
-            and item.startswith("http")
-            and "favicon" not in item
-        ]
-
-        lang_code, code = None, None
-        try:
-            lang_code = parsed_answer[4][0][1][0].split("```")[1].split("\n")[0].strip()
-            code = parsed_answer[4][0][1][0].split("```")[1][len(lang_code):]
-        except (IndexError, TypeError, KeyError, ValueError):
-            pass
-
-        answer = {
+        return {
             "content": parsed_answer[4][0][1][0],
             "conversation_id": parsed_answer[1][0],
             "response_id": parsed_answer[1][1],
-            "factuality_queries": parsed_answer[3],
-            "text_query": parsed_answer[2][0] if parsed_answer[2] else "",
             "choices": [{"id": x[0], "content": x[1]} for x in parsed_answer[4]],
-            "links": links,
-            "images": images,
-            "language_code": lang_code,
-            "code": code,
+            "images": (
+                {img[0][0][0] for img in parsed_answer[4][0][4]}
+                if parsed_answer[4][0][4]
+                else set()
+            ),
         }
-        return answer["content"]
 
     def _create_session(self):
         """
-        Create an HTTP session with necessary headers and cookies.
+        Create and configure an HTTP session for making requests.
 
         Returns:
-            Session: An instance of the requests.Session class.
+            requests.Session: A configured session object.
         """
         session = Session()
         session.headers = {
@@ -123,27 +85,29 @@ class Bard:
             "Referer": "https://bard.google.com/",
         }
         session.cookies.set("__Secure-1PSID", self.cookie)
+        session.cookies.set("__Secure-1PSIDCC", "")
+        session.cookies.set("__Secure-1PSIDTS", "")
         session.proxies = self.proxies
         return session
 
     def _get_snlm0e(self):
         """
-        Extract the SNlM0e value from the Secure_1PSID cookie.
+        Fetch and extract the 'SNlM0e' value from the website.
 
         Returns:
-            str: The extracted SNlM0e value.
+            str: The extracted 'SNlM0e' value.
 
         Raises:
-            Exception: If the cookie value is incorrect or if SNlM0e value is not found.
+            Exception: If the response code isn't 200 or 'SNlM0e' value is not found.
         """
-        if not self.cookie or self.cookie[-1] != ".":
-            raise Exception("Incorrect __Secure-1PSID cookie value.")
         resp = self.session.get(
             "https://bard.google.com/", timeout=30, proxies=self.proxies
         )
         if resp.status_code != 200:
-            raise Exception("Unable to fetch the response.")
+            raise exceptions.RequestException("Unable to fetch the response.")
         snlm0e = search(r"SNlM0e\":\"(.*?)\"", resp.text).group(1)
         if not snlm0e:
-            raise Exception("SNlM0e value not found in the given Secure_1PSID cookie.")
+            raise error(
+                "SNlM0e value not found in the given Secure_1PSID cookie value."
+            )
         return snlm0e
